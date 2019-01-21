8861da278c6d76c8735f56d3842f170ef4b5ef1e
Dylan Leigh <dylan@shepherd.home.dylanleigh.net>
2019-01-22 07:33:09 +1100

    showing daily target data on diary breakdown; bar charts still todo

--- pants/diary/views.py
+++ pants/diary/views.py
@@ -5,9 +5,11 @@ from django.views.generic.list import ListView
 from django.views.generic.base import TemplateView
 
 from .models import DiaryFood
+from targets.models import Target
 from ingredients.utils import add_nutrition_ratios
 
 class DiaryFoodListView(LoginRequiredMixin, ListView):
+   '''Flat diary list view showing all items in the past'''
    model = DiaryFood
 
    # ALWAYS filter to logged in user
@@ -21,6 +23,8 @@ class DiaryFoodListView(LoginRequiredMixin, ListView):
 
 
 class DiaryBreakdownView(LoginRequiredMixin, TemplateView):
+   '''Main /diary/ list view, split over this calendar day, last 24
+      hours, last calendar day.'''
 
    template_name='diary/diarybreakdown_list.html'
 
@@ -31,6 +35,10 @@ class DiaryBreakdownView(LoginRequiredMixin, TemplateView):
       # Get aggregate data for last 2 calendar days and add to context
       diarydata = DiaryFood.get_recent_diary_aggs(user)
       context.update(diarydata)
+
+      # Current daily target
+      daily_target = Target.get_primary_target(user)
+      context.update({'daily_target': daily_target})
 
       return context
 
