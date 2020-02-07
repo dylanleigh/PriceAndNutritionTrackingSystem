450925d530dba13719d7960c767f0467c42c840f
Dylan Leigh <dylan@shepherd.home.dylanleigh.net>
2020-02-07 20:33:00 +1100

    Revert "add twoday breakdown url entry (code to do it differently not there yet)"

--- pants/diary/urls.py
+++ pants/diary/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
    # /diary/ - default breakdown
    url(r'^$', views.DiaryBreakdownView.as_view(), name='diarybreakdown-list'),
    # /diary/flat/ - flat list view, original /diary/
-   url(r'^twoday/', views.DiaryBreakdownView.as_view(), name='diarybreakdowntwoday-list'),
-   # /diary/flat/ - flat list view, original /diary/
    url(r'^flat/', views.DiaryFoodListView.as_view(), name='diaryfood-list'),
 ]
 
