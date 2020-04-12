e1589e32ea46637793edb1906dc8e77437b0aeaa
Dylan Leigh <dylan@shepherd.home.dylanleigh.net>
2020-04-12 14:49:43 +1000

    Add nutrition_data to diaryfood API, as a read-only nested dictionary

--- pants/diary/serializers.py
+++ pants/diary/serializers.py
@@ -3,20 +3,18 @@ from rest_framework import serializers
 from .models import DiaryFood
 
 class DiaryFoodSerializer(serializers.HyperlinkedModelSerializer):
+   nutrition_data = serializers.ReadOnlyField()
+
    class Meta:
       model = DiaryFood
 
-      # Includes all fields, (inherits the nutrition fields from
-      # AbstractBaseNutrients), but not nutrition_data.
-      # FIXME Needs nutrition_data for display
-
+      # Includes all fields except user (already filtered to the
+      # logged in user).
       # Note DiaryFood entries of an Ingredient/Recipe are
-      # crystallized to the DiaryFood, so no need to recurse to them
-      # to get nutrition data (just the name or link to detail page, etc)
+      # crystallized to the DiaryFood, so returned nutrition data is
+      # of this object, not it's relations.
 
-      # Always the logged in user
       # FIXME re-include Ingredient/Recipe when they have API endpoints too
 
       exclude = ['user', 'of_recipe', 'of_ingredient']
-
 
