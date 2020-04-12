a56d2caa4aa220b689c52ab5ec0e814464861192
Dylan Leigh <dylan@shepherd.home.dylanleigh.net>
2020-04-12 14:20:50 +1000

    Paginate API to 20 items by default

--- pants/diary/serializers.py
+++ pants/diary/serializers.py
@@ -8,13 +8,15 @@ class DiaryFoodSerializer(serializers.HyperlinkedModelSerializer):
 
       # Includes all fields, (inherits the nutrition fields from
       # AbstractBaseNutrients), but not nutrition_data.
+      # FIXME Needs nutrition_data for display
+
       # Note DiaryFood entries of an Ingredient/Recipe are
       # crystallized to the DiaryFood, so no need to recurse to them
       # to get nutrition data (just the name or link to detail page, etc)
 
       # Always the logged in user
       # FIXME re-include Ingredient/Recipe when they have API endpoints too
-       
+
       exclude = ['user', 'of_recipe', 'of_ingredient']
 
 
--- pants/pants/settings.py
+++ pants/pants/settings.py
@@ -110,7 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 
 # Internationalization
