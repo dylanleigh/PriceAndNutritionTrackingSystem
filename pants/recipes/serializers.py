c8bf0f331c3bf7f1f32a3086c0bc560a72e86134
Dylan Leigh <dylan@shepherd.home.dylanleigh.net>
2020-04-14 12:58:57 +1000

    add tags and flags to recipe serializer (as string fields)

--- pants/recipes/serializers.py
+++ pants/recipes/serializers.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 from .models import Recipe
 
 class RecipeSerializer(serializers.HyperlinkedModelSerializer):
-   nutrition_data = serializers.ReadOnlyField()
-   owner = serializers.ReadOnlyField()
+   nutrition_data = serializers.ReadOnlyField()       # Calculated values
+   owner = serializers.ReadOnlyField()                # Current user or null, but immutable
+   tags = serializers.StringRelatedField(many=True)
+   flag = serializers.StringRelatedField()
 
    class Meta:
       model = Recipe
-      exclude = ['tags','flag']   # FIXME TODO need nested serialization
+      fields = '__all__'
