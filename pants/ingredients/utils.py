f957d8fff4cb4771850b8017aa2d6db13f587af2
Dylan Leigh <dylan@shepherd.home.dylanleigh.net>
2020-04-15 11:18:50 +1000

    Add IsOwnerOrGlobal permissions class for DRF - not used yet

--- pants/ingredients/utils.py
+++ pants/ingredients/utils.py
@@ -5,16 +5,37 @@ from collections import defaultdict
 from django.conf import settings
 from django.db.models import Q
 
+from rest_framework import permissions
+
 # Utility functions for ingredients/nutrient data
 
 THOUSAND=Decimal(1000)
 
+
 def owner_or_global(model, user):
    """
    Returns the objects of the model which are either owned by the
-   given user or global (null)
+   given user or global (null).
    """
+   # Use in place of e.g. Recipe.objects.filter( -> owner_or_global(Recipe,user).filter(
    return model.objects.filter(Q(owner__isnull=True)|Q(owner=user))
+
+# FIXME use this for finer grained api access control
+class IsOwnerOrGlobal(permissions.BasePermission):
+   """
+   Object-level permission to only allow owners of an object full
+   access to it, and read-only access to global (no owner) objects.
+   Assumes the model instance has an `owner` attribute.
+   """
+
+   def has_object_permission(self, request, view, obj):
+      # Read permissions are allowed to unowned objects,
+      # GET, HEAD or OPTIONS requests.
+      if request.method in permissions.SAFE_METHODS:
+         if obj.owner == None:
+            return True
+
+      return obj.owner == request.user
 
 
 # TODO This could be its own class that generates/caches individual
