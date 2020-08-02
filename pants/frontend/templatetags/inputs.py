ec9b7c760e9cb222a3b4bef849a5cf60ae730f6c
Austin Crapo <corey.crapo@gmail.com>
2020-08-02 15:40:55 -0600

    Add reset css, improve readability, drop prefixes

--- pants/frontend/templatetags/inputs.py
+++ pants/frontend/templatetags/inputs.py
@@ -4,14 +4,16 @@ register = template.Library()
 
 
 @register.inclusion_tag("frontend/templatetags/float_input.html")
-def float_input(input_type, label, name=None, multiline=False, hint=""):
+def float_input(label, name=None, multiline=False, hint="", extra="", input_type="text"):
     """
     hint is a paragraph explaining what could be put in, or limitations to inputs. Possibly should be made visible on focus
+    extra is string that will be added to the attributes area of the input, useful for disabling inputs for example
     """
     return {
         'type': input_type,
         'label': label,
         'multiline': multiline,
         'name': name,
-        'hint': hint
+        'hint': hint,
+        'extra': extra
     }
