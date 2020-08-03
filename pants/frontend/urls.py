02c0009f537401fd573e816b6525681945eabfe0
Austin Crapo <corey.crapo@gmail.com>
2020-08-03 00:36:16 -0600

    Add recipe manager page

--- pants/frontend/urls.py
+++ pants/frontend/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ingredient_manager)
+    url(r'^$', views.ingredient_manager),
+    url(r'^recipe$', views.recipe_manager)
 ]
--- pants/frontend/views.py
+++ pants/frontend/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-import json
 
 def ingredient_manager(request):
-    return render(request, 'frontend/ingredient_manager.html', {})
+    return render(request, 'frontend/ingredient_manager.html')
+
+def recipe_manager(request):
+    return render(request, 'frontend/recipe_manager.html')
 
