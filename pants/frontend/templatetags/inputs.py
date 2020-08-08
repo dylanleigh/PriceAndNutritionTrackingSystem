9a9d183d09cb445b20d94ee6d3cb86785801bc44
Austin Crapo <corey.crapo@gmail.com>
2020-08-07 20:46:53 -0600

    Convert float_input template tag into float-input custom HTML element. Convert Ingredient Manager to use float-input
    Cleaner design, greater js control

--- pants/frontend/templatetags/inputs.py
+++ pants/frontend/templatetags/inputs.py
@@ -6,10 +6,10 @@ register = template.Library()
 @register.inclusion_tag("frontend/templatetags/float_input.html")
 def float_input(label, name=None, multiline=False, hint="", extra="", input_type="text", input_mask=None, container_extra=""):
     """
-    hint is a paragraph explaining what could be put in, or limitations to inputs. Possibly should be made visible on focus
+
     extra is string that will be added to the attributes area of the input, useful for disabling inputs for example
     container_extra is like extra, but for the container
-    input mask is for js input masking options with IMask library.
+
     """
     return {
         'type': input_type,
