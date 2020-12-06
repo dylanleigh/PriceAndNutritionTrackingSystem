a95d0bec656e8a8dca0c8192aa5a1eaec4ab8bfb
Austin Crapo <corey.crapo@gmail.com>
2020-12-06 11:21:37 -0700

    Experimenting with a vue front end project

---
+++ trouser/src/components/pages/ingredient-manager.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>
+        Hello World And stuff
+    </p>
+</template>
+
+<script>
+    export default {
+        name: "ingredient-manager"
+    }
+</script>
+
+<style scoped>
+
+</style>
